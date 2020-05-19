a954548ca6841b2648ad2aba324caf9aaaebfefa
ItoYo16u <ito.yo16uh90616@gmail.com>
2020-05-19 18:01:25 +0900

    add method to check overrapped time range between two

--- src/main/scala/class_usages/time/TimeRange.scala
+++ src/main/scala/class_usages/time/TimeRange.scala
@@ -8,6 +8,20 @@ case class TimeRange(start: Time, end:Time){
     start <= target && target <= end
   }
 
+  def hasIntersection(that:TimeRange): Boolean = {
+    if(this.start <= that.start) {
+      that.start < this.end
+    } else {
+      this.start < that.end
+    }
+  }
+
   override def toString: String = "%s to %s".format(start,end)
 
+}
+
+object TimeRange {
+  def apply(start:String,end:String) = {
+    new TimeRange(Time(start),Time(end))
+  }
 }
