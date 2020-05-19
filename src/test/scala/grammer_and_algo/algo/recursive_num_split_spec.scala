7d9befd5a737c124cf488297be5157f9e6a3dc8c
ItoYo16u <ito.yo16uh90616@gmail.com>
2020-05-19 20:46:42 +0900

    update test

--- src/test/scala/grammer_and_algo/algo/recursive_num_split_spec.scala
+++ src/test/scala/grammer_and_algo/algo/recursive_num_split_spec.scala
@@ -5,9 +5,11 @@ class RecursiveNumSplitSpec extends FunSpec
   with Matchers
   with GivenWhenThen {
   describe("recursive_num_split(n:Int)"){
-    it("splits a number into a list of 1 digit until their sum becomes 2 digits"){
-      Then("returns (2 digit number + each digit of it)")
-
+    it("splits a number into a list composed of each digit of the number until their sum becomes 1 digit"){
+      Then("returns the sum of 1 digit number and the number from which 1 digit number was calculated ")
+      // 98 -> 9+8 = 17
+      // 17 -> 1+7 = 8
+      // 17 + 8 = 25
       RecursiveMethod.num_split(98) shouldBe 25
       RecursiveMethod.num_split(3528) shouldBe 27
       RecursiveMethod.num_split(9514599) shouldBe 48
