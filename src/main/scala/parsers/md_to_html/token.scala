abe7e4b5c42a1dd975a01698694b8aa1654db18b
ItoYo16u <ito.yo16uh90616@gmail.com>
2020-06-04 02:50:46 +0900

    add block elm and tes spec

--- src/main/scala/parsers/md_to_html/token.scala
+++ src/main/scala/parsers/md_to_html/token.scala
@@ -17,7 +17,6 @@ abstract trait InlineTag extends Inline{
     }
   }
 }
-
 case class Tag(value:String,options:Map[String,String]=Map.empty[String,String]) {
   val optionsAsString = options.iterator.foldLeft(""){
     case (acc,(k,v))=> {
@@ -25,13 +24,32 @@ case class Tag(value:String,options:Map[String,String]=Map.empty[String,String])
     }
   }
   def toHTMLTags:(String,String) = (s"<$value $optionsAsString>",s"</$value>")
+  def toHTMLTag:String = s"<$value $optionsAsString/>"
 }
-
-
-
 // Inline element
 case class Strong(content:Inline) extends InlineTag{val tag = Tag("strong")}
 case class Emphasis(content:Inline) extends InlineTag{val tag=Tag("em")}
 case class Italic(content:Inline) extends InlineTag{val tag=Tag("i")}
 case class Link(content:Inline,href:String) extends InlineTag{val tag=Tag("a",options = Map("href"->href))}
 case class PlainText(content:String) extends InlineText
+
+
+sealed abstract trait Block{
+  def tag:Tag
+}
+
+abstract trait BlockContent extends Block {
+  def content: Inline
+  override def toString: String = {
+    tag.toHTMLTags match {
+      case (start,end) => start + content.toString + end
+    }
+  }
+}
+
+case class Heading(level:Int,content:Inline) extends BlockContent{
+  val tag=Tag(s"h$level")
+}
+case class P(content:Inline) extends BlockContent{val tag=Tag("p")}
+case object Empty extends Block{val tag=Tag("")}
+case object HR extends Block{val tag=Tag("hr")}
--- src/test/scala/parsers/md_to_html/token_spec.scala
+++ src/test/scala/parsers/md_to_html/token_spec.scala
@@ -1,5 +1,5 @@
 import org.scalatest.{FunSpec, Matchers}
-import com.github.ItoYo16u.parsers.md_to_html.{PlainText, Strong,Link,Emphasis}
+import com.github.ItoYo16u.parsers.md_to_html.{PlainText, Strong,Link,Emphasis,Heading}
 
 class TokenSpec extends FunSpec with Matchers {
 
@@ -19,6 +19,15 @@ class TokenSpec extends FunSpec with Matchers {
     it("returns HTML"){
       val strongEmphasis = Strong(Emphasis(PlainText("test")))
       strongEmphasis.toString shouldBe "<strong ><em >test</em></strong>"
+    }
+  }
+  describe("Heading(n)"){
+    it("returns <hn>content</hn>"){
+      val headings: List[Heading]  = (1 to 6).map((n)=>Heading(n,Strong(PlainText("test")))).toList
+      headings.zipWithIndex.foreach{case(headingN,idx)=>{
+        println(headingN.toString)
+        headingN.toString shouldBe s"<h${idx+1} ><strong >test</strong></h${idx+1}>"
+      }}
     }
   }
 
