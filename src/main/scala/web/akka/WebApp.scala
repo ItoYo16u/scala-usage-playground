e5d67c83edcc9e70e98fffe53772f7536fe3bdfb
ItoYo16u <ito.yo16uh90616@gmail.com>
2020-09-04 02:42:10 +0900

    add stream usage

--- src/main/scala/web/akka/WebApp.scala
+++ src/main/scala/web/akka/WebApp.scala
@@ -3,17 +3,20 @@ package web.akka
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
+import akka.stream.scaladsl._
 import akka.http.scaladsl.model.HttpMethods._
 import akka.http.scaladsl.model._
+import akka.util.ByteString
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 import scala.io.StdIn
+import scala.util.Random
 
 object WebApp {
   def main(args: Array[String]): Unit = {
-    implicit val system = ActorSystem(Behaviors.empty,"webApp")
+    implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty,"webApp")
     implicit val executionContext:ExecutionContext = system.executionContext
-
+    val numbers = Source.fromIterator(()=> Iterator.continually(Random.nextInt()))
     val requestHandler : HttpRequest => HttpResponse = {
             // uri,path,header,entity,protocol
       case HttpRequest(GET,Uri.Path("/"),_,_,_)=>HttpResponse(
@@ -23,7 +26,15 @@ object WebApp {
           string = "{'status':'success'}"
         )
       )
-      case r: HttpRequest => HttpResponse(404)
+      case HttpRequest(GET,Uri.Path("/random"),_,_,_) => HttpResponse(
+        entity = HttpEntity.apply(
+          contentType = ContentTypes.`text/plain(UTF-8)`,
+          numbers.map(n=>ByteString(s"$n\n"))
+        )
+      )
+      case r: HttpRequest =>
+        r.discardEntityBytes()
+        HttpResponse(404)
         // これが無いとInternalServerErrorになる
     }
     val bindingFuture = Http().newServerAt("localhost",8080).bindSync(requestHandler)
