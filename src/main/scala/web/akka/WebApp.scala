e76a1fb6d4287fe3c8dce3d1ef3b3adba58be35c
ItoYo16u <ito.yo16uh90616@gmail.com>
2020-09-05 02:22:17 +0900

    add broadcasting messages

--- src/main/scala/web/akka/WebApp.scala
+++ src/main/scala/web/akka/WebApp.scala
@@ -3,12 +3,19 @@ package web.akka
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
+
+import scala.concurrent.duration._
 import akka.stream.scaladsl._
+import akka.http.scaladsl.server.directives._
+import akka.http.javadsl.model.ws.Message
 import akka.http.scaladsl.model.HttpMethods._
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.ws.{BinaryMessage, TextMessage, WebSocketUpgrade}
+import akka.stream.KillSwitches
 import akka.util.ByteString
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration.Duration
+import scala.concurrent.{ExecutionContext, Future, duration}
 import scala.io.StdIn
 import scala.util.Random
 
@@ -16,7 +23,9 @@ object WebApp {
   def main(args: Array[String]): Unit = {
     implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty,"webApp")
     implicit val executionContext:ExecutionContext = system.executionContext
+
     val numbers = Source.fromIterator(()=> Iterator.continually(Random.nextInt()))
+    val (sink,source) = MergeHub.source[Message].map(req => req).toMat(BroadcastHub.sink[Message])(Keep.both).run()
     val requestHandler : HttpRequest => HttpResponse = {
             // uri,path,header,entity,protocol
       case HttpRequest(GET,Uri.Path("/"),_,_,_)=>HttpResponse(
@@ -32,6 +41,29 @@ object WebApp {
           numbers.map(n=>ByteString(s"$n\n"))
         )
       )
+
+      case req @ HttpRequest(GET, Uri.Path("/join"), _, _,_) =>
+        req.attribute(AttributeKeys.webSocketUpgrade) match {
+            // プロトコルの更新
+          case Some(upgrade: WebSocketUpgrade) => upgrade.handleMessages{
+            val queryString = req.uri.rawQueryString.getOrElse("nothing")
+            println(queryString)
+            Flow.fromSinkAndSource(sink,source).mapConcat{
+              case tm:TextMessage =>   tm :: tm :: Nil
+              case bm: BinaryMessage => bm.dataStream.runWith(Sink.ignore)
+                Nil
+            }.throttle(1,1.seconds)
+        /*  Flow[Message].mapConcat{
+              case tm: TextMessage => (0 to 100).map(_ => tm)
+              //TextMessage(Source.single(("response:")) ++ tm.textStream) :: Nil
+              case bm: BinaryMessage =>
+                // ignore binary messages but drain content to avoid the stream being clogged
+                bm.dataStream.runWith(Sink.ignore)
+                Nil
+            }.throttle(1,1.seconds)*/
+          }
+          case None => HttpResponse(400,entity="invalid websocket request")
+        }
       case r: HttpRequest =>
         r.discardEntityBytes()
         HttpResponse(404)
@@ -42,3 +74,4 @@ object WebApp {
     bindingFuture.flatMap(_.unbind()).onComplete(_=>system.terminate())
   }
 }
+
---
+++ src/main/scala/web/akka/graph/X3.scala
@@ -0,0 +1,19 @@
+package web.akka.graph
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Flow,Sink,Source}
+
+// Flowはフィルター, Sinkは入り口,Sourceは出口,の抽象化
+
+object X3 {
+  implicit val actorSystem =  ActorSystem()
+  implicit val mat = ActorMaterializer()
+
+  def main(args:Array[String]): Unit = {
+    val source = Source(1 to 100)
+    val flow = Flow.fromFunction[Int,Int](_*3)
+    val sink = Sink.foreach(println)
+    val graph = source.via(flow).to(sink)
+    graph.run()
+  }
+}
